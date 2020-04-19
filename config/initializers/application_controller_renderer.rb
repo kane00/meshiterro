85b6caf288e8f76cf90fdcad27553f8b4c9e241c
test <test@tesutonoMacBook-ea.local>
2020-04-19 17:42:41 +0900

    [finish]6-8

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '19c438936ee72154fd19d3577f046191bf3aa07fa5800da60d25db19d0a03a8b1ddaac09eaa3f9d73a69c755c3edf49ba3db808fe33ed75d821afc368a91dcee'
